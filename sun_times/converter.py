7ce0ed4cf9ff804504f8e350de638ad11e862737
Christian Ege <nereus@familie-ege.net>
2011-01-02 20:52:55 +0100

    renamed some files

--- sun_times/converter.py
+++ sun_times/converter.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 
 import csv
+import os
 
-reader = csv.DictReader(open("november.csv", "rb"), delimiter=",")
+csv_filenames = []
 
-for row in reader: 
-        print row
+for dirname, dirnames, filenames in os.walk('.'):
+	for filename in filenames:
+		if True == filename.endswith("csv"):
+			print os.path.join(dirname, filename)
+			csv_filenames.append(os.path.join(dirname, filename))	
+
+dum = sorted(csv_filenames)
+
+sunrise = []
+sunset = []
+
+for i in dum:
+	print i
+	reader = csv.DictReader(open(i, "rb"), delimiter=",")
+	for row in reader: 
+		print row
+		sunrise.append(row['Sonnenaufgang'])
+		sunset.append(row['Sonnenuntergang'])
+print sunrise
